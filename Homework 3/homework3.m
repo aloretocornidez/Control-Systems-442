f9a6cb6107c5f259f5ec7b4e6008ea5cf96d9cd8
Alan Manuel <aloretocornidez@email.arizona.edu>
2022-02-27 09:24:51 -0700

    Update homework3.m

--- Homework 3/homework3.m
+++ Homework 3/homework3.m
@@ -6,11 +6,18 @@ Assignemnent: Homework 3
 
 %}
 
-n = [1];
-d = [1 1 0];
-sampleTime = 0.02
+pkg load control;
+pkg load signal;
 
-g = tf(n, d)
+clc
+
+num = [ 3600 ]
+den = [ 1 84.853 3600 ]
+sampleTime = 0.0035
+frequency = 1 / sampleTime
+
+
+g = tf(num, den)
 
 
 
@@ -19,5 +26,11 @@ z = c2d(g, sampleTime)
 analogPoles = pole(g)
 digitalPoles = pole(z)
 
+
 analogZeros = zero(g)
 digitalZero = zero(z)
+
+
+
+
+
